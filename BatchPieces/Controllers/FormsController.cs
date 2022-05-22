6ecb27b82190cc14f2a1338a985ccc67f5a84d7c
mayabin2527 <846209981@qq.com>
2022-05-22 20:15:08 +0800

    添加根据List数据动态生成前端datatable

--- BatchPieces/Controllers/FormsController.cs
+++ BatchPieces/Controllers/FormsController.cs
@@ -202,7 +202,8 @@ return ret;
 public JsonResult myData(string username, string age)
 {
 List<Employee> list = new List<Employee>();
-for (int i = 1; i <= 50; i++)
+List<TableList> list1 = new List<TableList>();
+            for (int i = 1; i <= 10; i++)
 {
     string ID = i.ToString();
     string First = "First" + i.ToString();
@@ -212,8 +213,11 @@ for (int i = 1; i <= 50; i++)
     string sa = " <a id = '{0}' href = '#' class='fa fa-edit edit1' data-toggle='modal' data-target='.bs-example-modal-lg'>编辑</a>  <a id = '{1}' href='#' class='fa fa-times delete1' data-toggle='modal' data-target='.bs-example-modal-sm'>删除</a>";
     string Salary = string.Format(sa, "edit" + i.ToString(), "deltel" + i.ToString());
     list.Add(new Employee() { ID = ID, First = First, LastName = LastName, Username = Salary });
+
+     list1.Add(new TableList() { 编号 = ID, 姓什么 = First, 名字 = LastName, 用户名称 = Salary });
+
 }
-return Json(new { data = " <tr>  <td> 5</td> <td>Larry5</td> <td>the Bird5</td>   <td>twitter5</td> </tr >", State = "Ok" }, JsonRequestBehavior.AllowGet);
+            return Json(new { data = " <tr>  <td> 5</td> <td>Larry5</td> <td>the Bird5</td>   <td>twitter5</td> </tr >",List= list1, State = "Ok" }, JsonRequestBehavior.AllowGet);
 }
 public class Employee
 {
@@ -223,5 +227,14 @@ public string First { get; set; }
 public string LastName { get; set; }
 public string Username { get; set; }
 }
-}
+
+        public class TableList
+        {
+
+            public string 编号 { get; set; }
+            public string 姓什么 { get; set; }
+            public string 名字 { get; set; }
+            public string 用户名称 { get; set; }
+        }
+    }
 }
